4e9385e436b8e0e6fb1120e5572014bd954c312a
UltralyticsAssistant <web@ultralytics.com>
2025-09-08 04:24:45 +0000

    Auto-format by https://ultralytics.com/actions

--- simplify_onnx.py
+++ simplify_onnx.py
@@ -2,7 +2,7 @@ import onnx
 from onnxsim import simplify
 
 # 加载预定义的 ONNX 模型
-model = onnx.load('yolov11n.onnx')
+model = onnx.load("yolov11n.onnx")
 
 # 简化模型
 model_simp, check = simplify(model)
@@ -11,4 +11,4 @@ model_simp, check = simplify(model)
 assert check, "Simplified ONNX model could not be validated"
 
 # 保存简化后的模型
-onnx.save(model_simp, 'yolov11n-sim.onnx')
+onnx.save(model_simp, "yolov11n-sim.onnx")
