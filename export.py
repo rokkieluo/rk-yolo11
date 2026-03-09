f890e1a591cd7ed0aa0fa5a4de57b1c642261642
UltralyticsAssistant <web@ultralytics.com>
2026-03-09 04:12:40 +0000

    Auto-format by https://ultralytics.com/actions

--- export.py
+++ export.py
@@ -1,3 +1,4 @@
 from ultralytics import YOLO
-model = YOLO(model='yolo11n.pt')  # load a pretrained model (recommended for training)
-results = model(task='detect', source='./test.jpg', save=True)  # predict on an image
+
+model = YOLO(model="yolo11n.pt")  # load a pretrained model (recommended for training)
+results = model(task="detect", source="./test.jpg", save=True)  # predict on an image
